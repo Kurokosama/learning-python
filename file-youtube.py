1d77a814aa3e2d2b519081c52b1add5edda90bf2
Shirai Kuroko <57276534+Kurokosama@users.noreply.github.com>
2021-10-13 09:15:28 -0500

    day2

--- file-youtube.py
+++ file-youtube.py
@@ -1,4 +1,19 @@
+"""
 num1 = input("enter your number1:  ")
 num2 = input("enter your number2： ")
 result = float(num1) + float(num2)
 print(result)
+"""
+
+numbers = [1,2,3,4,5]
+people = ["Kevin", "Sam", "Karen", "Oman"]
+
+people.extend(numbers)
+
+people.append("SB")
+
+people.insert(1, "USB")
+
+people.remove("Sam")
+print(people)
+print(people.index("SB"))0
