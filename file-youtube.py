a71c3d12c6cb2443e62d52de8839fb00a64dae6b
Shirai Kuroko <57276534+Kurokosama@users.noreply.github.com>
2021-10-22 01:44:30 -0500

    day3

--- file-youtube.py
+++ file-youtube.py
@@ -27,3 +27,47 @@ print ("tup1[0]: ", tup1[0])
 print ("tup2[1:5]: ", tup2[1:5])
 
 #Functions
+
+a_name = "Kuroko"
+a_age = "25"
+
+def sayhi(name, age):
+    print("Hello " + name + " You are: " + age)
+
+
+sayhi(a_name, a_age)
+
+def cube(num):
+    return num*num*num
+
+print(cube(3))
+
+print("*********************************")
+#if statement
+is_male = False
+is_tall = False
+
+if is_male and is_tall:
+    print("You are tall male")
+elif is_male and not(is_tall):
+    print("You are not tall")
+elif not(is_male) and is_tall:
+    print("You are tall not male")
+else:
+    print("You are not male and tall")
+
+print("*********************************")
+
+num1 = float(input("enter num1  "))
+num2 = float(input("enter num2  "))
+num3 = float(input("enter num3  "))
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print("The MAX is: " + str(max_num(num1, num2, num3)))
